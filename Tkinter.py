7853fdc2a27b6be1aad8e7cfd12ce5743a095609
Caleb Sinclair <calebrsinclair@gmail.com>
2026-04-28 13:34:24 +1200

    Added some buttons to the tkinter window and set up the button that will open the PyGame visualization of the climbing wall.

--- Tkinter.py
+++ Tkinter.py
@@ -1,7 +1,33 @@
 import tkinter as tk
+import json
 
+#Opens the main tkinter window and creates all the button text etc.
 def TkinterUI():
+    from main import PyGame
+    global root
     root = tk.Tk()
-    root.geometry("1000x1000")
+    root.geometry("500x500")
+
+    InventoryButton = tk.Button(text = "Inventory", command = InventoryWindow)
+    InventoryButton.pack()
+
+    VisualizeButton = tk.Button(text = "Visualize", command = PyGame)
+    VisualizeButton.pack()
 
     root.mainloop()
+
+#Opens the inventory window
+def InventoryWindow():
+    global root
+    InventoryWindow = tk.Toplevel(root)
+    InventoryWindow.geometry("500x500")
+
+
+    InventoryTitle = tk.Label(InventoryWindow, text = "Inventory")
+    InventoryTitle.pack()
+
+
+TkinterUI()
+
+
+
--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-import Tkinter
+#Run the PyGame script in this function
+def PyGame():
+    print("PyGame")
 
-Tkinter.TkinterUI()
+#This function runs the tkinter file and opens the tkinter window
+#BUG the run tkinter runs mutiple times after closing the window
+def RunTkinter():
+    import Tkinter
+    Tkinter.TkinterUI()
+
+RunTkinter()
