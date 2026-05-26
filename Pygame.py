b8e4850d4a411aa283504f0eed0a2446070db441
Albie Denley <Albie.denley@gmail.com>
2026-05-26 12:39:03 +1200

    It is broken now

--- Pygame.py
+++ Pygame.py
@@ -1,120 +1,102 @@
 import pygame as py
 import os, json
 
-# Initialise py
 py.init()
 
 num_holds = {}
 hold_info = {}
-hold_pos = {}
-wall_1_holds = {}
-wall_1_hold_info = {}
-
+Inventory = {}
 dicts = {
-    "num_holds" : num_holds,
-    "hold_info" : hold_info,
-    "hold_pos" : hold_pos
+    "num_holds": num_holds,
+    "hold_info": hold_info,
+    "Inventory": Inventory
 }
 
 ScriptDir = os.path.dirname(os.path.abspath(__file__))
-FilePath = os.path.join(ScriptDir, "num_holds.json")
+FilePath = os.path.join(ScriptDir, "Inventory.json")
 
 if os.path.exists(FilePath):
-        try:
-            with open(FilePath, 'r') as f:
-                Data = json.load(f)
-                for key in Data:
-                    for i in Data[key]:
-                        dicts[key].update({i:Data[key][i]})
-        except:
-            pass
-print(num_holds)
-for i in num_holds:
-    for j in range(num_holds[i]):
-        hold_pos.update({i + "_" + j:(hold_info[i][0], hold_info[i][1])})
-print(hold_pos)
+    try:
+        with open(FilePath, 'r') as f:
+            Data = json.load(f)
+            for key in Data:
+                for i in Data[key]:
+                    dicts[key].update({i: Data[key][i]})
+    except Exception as e:
+        print(f"Failed to load save: {e}")
 
-
-#Variables
-mouse_down = False
-offset = 0
-does_offset = 1
-mouse_offset = 0
-wall_spots = []
+def save_data():
+    with open(FilePath, 'w') as f:
+        json.dump({
+            "num_holds": num_holds,
+            "hold_info": hold_info,
+            "Inventory": Inventory
+        }, f, indent=2)
 
 def wall_1():
-    
-    does_offset = 1
+    global num_holds
 
-    # Screen setup
     WIDTH, HEIGHT = 500, 600
     screen = py.display.set_mode((WIDTH, HEIGHT))
     py.display.set_caption("Pygame Window")
 
-    holds_used = {}
     holding_index = None
     y_hole_distance = 25
-    x_hole_distance = 2*y_hole_distance
-
+    x_hole_distance = 25
+    offset = x_hole_distance / 2
+    wall_spots = []
     for n in range(HEIGHT - 100):
-            if n % y_hole_distance == 0:
-                does_offset += 1
-                for i in range(WIDTH):
-                    if i % x_hole_distance == 0:
-                        if does_offset % 2 == 0:
-                            offset = x_hole_distance - x_hole_distance/4
-                        else:
-                            offset = x_hole_distance/4
-                        wall_spots.append((i + offset, n + y_hole_distance/2))
+        if n % y_hole_distance == 0:
+            for i in range(WIDTH):
+                if i % x_hole_distance == 0:
+                    
+                    wall_spots.append((i + offset, n + y_hole_distance / 2))
 
-    # Main loop
     running = True
     while running:
-        screen.fill("black")  # background colour
+        screen.fill("black")
         mouse_x, mouse_y = py.mouse.get_pos()
-        # --- Events ---
+
         for event in py.event.get():
             if event.type == py.QUIT:
+                save_data()  # Save on quit, not every frame
                 running = False
+
             elif event.type == py.MOUSEBUTTONDOWN:
-                for i in num_holds:
-                    x, y, r = hold_info[i][0], hold_info[i][1], hold_info[i][2]
-                    if (mouse_x - x)**2 + (mouse_y - y)**2 <= r**2:
+                for i in reversed(list(Inventory)):
+                    x, y, r = Inventory[i][0], Inventory[i][1], Inventory[i][2]
+                    if (mouse_x - x) ** 2 + (mouse_y - y) ** 2 <= r ** 2:
                         holding_index = i
                         break
-                
+
             elif event.type == py.MOUSEBUTTONUP:
+                if holding_index is not None:
+                    save_data()  # Save when you drop a hold
                 holding_index = None
 
         if holding_index is not None:
-            hold_info[holding_index][0] = mouse_x
-            hold_info[holding_index][1] = mouse_y
+            Inventory[holding_index][0] = mouse_x
+            Inventory[holding_index][1] = mouse_y
 
-        for i in num_holds:
-            x, y, r = hold_info[i][0], hold_info[i][1], hold_info[i][2]
-            if y <= 500:
-                num_holds.update({i:num_holds[i] - 1})
-                wall_1_hold_info.update({i:hold_info[i]})
-                if i in wall_1_holds:
-                    wall_1_holds[i] += 1
-                else:
-                    wall_1_holds.update({i:1})
-        
-
-        # if mouse_y % x_hole_distance < x_hole_distance/2:
-        #     mouse_offset = x_hole_distance - x_hole_distance/4
-        # else:
-        #     mouse_offset = x_hole_distance/4        
-
-        #Drawing
+        # Draw tray
         py.draw.rect(screen, "gray", py.Rect(0, HEIGHT - 100, WIDTH, 100))
 
-        for i in wall_spots:
-            py.draw.circle(screen, "red", i, 2)
+        # Draw holds using Inventory for position (Inventory just tracks what exists)
+        for i in Inventory:
+            x, y, r, colour, type = Inventory[i][0], Inventory[i][1], Inventory[i][2], Inventory[i][3], Inventory[i][4]
+            if y >= 500 and holding_index != i:
+                y = 550
+                x = hold_info[i[:-2]][0]
+            else:
+                y = y - (y % y_hole_distance) + y_hole_distance/2
+                x = x - (x % x_hole_distance) + x_hole_distance/2
+                
+            py.draw.circle(screen, colour, (x, y), r)
+            Inventory[i][0], Inventory[i][1], Inventory[i][2], Inventory[i][3], Inventory[i][4] = x, y, r, colour, type
 
-        for i in num_holds:
-                for j in range(num_holds[i]):
-                    py.draw.circle(screen, hold_info[i][3], (hold_info[i][0], hold_info[i][1]), hold_info[i][2])
+        # Draw wall hole spots
+        for spot in wall_spots:
+            py.draw.circle(screen, "red", spot, 2)
 
         py.display.flip()
 
